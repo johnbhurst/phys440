faae7c16535eb2086e749670d05c559f2ec657ca
John Hurst <john.b.hurst@gmail.com>
2023-12-16 10:21:49 +1300

    fix: Made turing.fsx more robust, extending tape with blanks when needed.

--- turing.fsx
+++ turing.fsx
@@ -19,14 +19,16 @@ let matchRule (machine:Machine) (rule:Rule) =
     machine.State = rule.State && machine.Tape.[machine.Position] = rule.Symbol
 
 let applyRule (machine:Machine) (rule:Rule) =
+    let newTape = List.updateAt machine.Position rule.NewSymbol machine.Tape
+    let newPosition = machine.Position + rule.Direction
     { State = rule.NewState;
-      Tape = List.updateAt machine.Position rule.NewSymbol machine.Tape;
-      Position = machine.Position + rule.Direction }
+      Tape = if newPosition >= newTape.Length then newTape @ ["blank"] else newTape;
+      Position = newPosition }
 
 let program = System.IO.File.ReadLines(fsi.CommandLineArgs.[1])
             |> Seq.map parseRule
 
-let tape = List.concat [("start" :: List.skip 2 (List.ofSeq fsi.CommandLineArgs)); ["blank"; "blank"; "blank"]]
+let tape = "start" :: List.skip 2 (List.ofSeq fsi.CommandLineArgs)
 
 let iterate (machine:Machine) =
     if machine.State = "qh" then None
