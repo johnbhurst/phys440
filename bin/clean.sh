c1d9e97b9d6b549602251b20124ff2fb3d743b5e
John Hurst <john.b.hurst@gmail.com>
2024-03-09 07:51:34 +1300

    feat: Added cleanup script.

---
+++ bin/clean.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Copyright 2024 John Hurst
+# John Hurst (john.b.hurst@gmail.com)
+# 2024-03-09
+
+# clean up temporary LaTeX files
+
+rm *.aux
+rm *.bbl
+rm *.bcf
+rm *.blg
+rm *.fdb_latexmk
+rm *.fls
+rm *.log
+rm *.out
+rm *.run.xml
+rm *.synctex.gz
+rm *.toc
