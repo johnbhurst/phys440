41d82363d4a98270e2a11a711ba64e575373805d
John Hurst <john.b.hurst@gmail.com>
2024-04-25 17:52:03 +1200

    feat: Refined program to make generic.

--- bin/homework12_q2_fakenoise_results.py
+++ bin/homework12_q2_fakenoise_results.py
@@ -2,25 +2,23 @@
 # Copyright 2024 John Hurst
 # John Hurst (john.b.hurst@gmail.com)
 
-import pandas as pd
+import argparse
 import matplotlib.pyplot as plt
-
+import pandas as pd
 import sys
 
-simulator_filename = sys.argv[1]
-kyoto_filename = sys.argv[2]
-osaka_filename = sys.argv[3]
+parser = argparse.ArgumentParser(description='Plot histograms of C values from CSV files.')
+parser.add_argument('--filename', type=str, help='Filename for histogram')
+parser.add_argument('--size', type=str, help='Size of histogram')
+parser.add_argument('files', metavar='file', type=str, nargs='+', help='CSV files to plot')
+args = parser.parse_args()
 
-# Read the 'c' column from each file
-simulator_data = pd.read_csv(simulator_filename, header=None, usecols=[5])
-kyoto_data = pd.read_csv(kyoto_filename, header=None, usecols=[5])
-osaka_data = pd.read_csv(osaka_filename, header=None, usecols=[5])
+plt.figure(figsize=(20, 6))
+for filename in args.files:
+    data = pd.read_csv(filename, header=None, usecols=[5])
+    plt.hist(data[5], alpha=0.5, label=filename.replace('.csv', ''))
 
-# Plot the histograms
-plt.hist(simulator_data[5], alpha=0.5, label='Simulator')
-plt.hist(kyoto_data[5], alpha=0.5, label='FakeKyoto')
-plt.hist(osaka_data[5], alpha=0.5, label='FakeOsaka')
-
+# Set the size of the plot
 # Set the title and labels
 # plt.title('Distributions of C')
 # plt.xlabel('c')
@@ -29,6 +27,7 @@ plt.hist(osaka_data[5], alpha=0.5, label='FakeOsaka')
 # Show the legend
 plt.legend(loc='upper right')
 
-# Display the plot
-# plt.show()
-plt.savefig('histogram.png')
+if args.filename:
+    plt.savefig(args.filename)
+else:
+    plt.show()
