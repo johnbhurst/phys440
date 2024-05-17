4482ea8e9ab0269fc0886707ec810329b956cc73
John Hurst <john.b.hurst@gmail.com>
2024-05-18 10:55:42 +1200

    feat: Match qubit conventions in Wong; added --qft alternative QFT implementation.

--- bin/homework3_q2_qpe_qiskit.py
+++ bin/homework3_q2_qpe_qiskit.py
@@ -10,14 +10,16 @@ import argparse
 import math
 from qiskit import  ClassicalRegister, QuantumCircuit, QuantumRegister
 from qiskit_aer import AerSimulator
+from qiskit.circuit.library import QFT
 from qiskit_ibm_runtime import SamplerV2 as Sampler
 from qiskit_ibm_runtime.fake_provider import FakeProviderForBackendV2 as FakeProvider
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 from qiskit.quantum_info import Operator
 
 parser = argparse.ArgumentParser(description='PHYS440 Homework 3 Q2: Quantum Phase Estimation.')
-parser.add_argument("--operator", type=str, default="Z", help="Operator (Z, Y, RX), (default Z)")
+parser.add_argument("--operator", type=str, default="Z", help="Operator (Z, Y), (default Z)")
 parser.add_argument("--eigenvector", type=str, default="e0", help="Eigenvector (e0, e1), (default e0)")
+parser.add_argument("--qft", action="store_true", help="Apply QFT builtin")
 parser.add_argument("--provider", type=str, default="aer", help="Provider (aer, fake_manila, fake_kyoto, etc), (default aer)")
 parser.add_argument("--shots", type=int, default=1024, help="Number of shots")
 parser.add_argument("--filename", type=str, help="Filename for circuit diagram")
@@ -30,33 +32,36 @@ circuit = QuantumCircuit(qubits, clbits)
 
 # Setup:
 if args.operator == "Z":
-    if args.eigenvector == "e1":
-        circuit.x(2)
+    if args.eigenvector == "e0":
+        circuit.x(0)
 elif args.operator == "Y":
     if args.eigenvector == "e0":
-        circuit.rx(math.pi/2, 2)
+        circuit.rx(math.pi/2, 0)
     else:
-        circuit.rx(-math.pi/2, 2)
+        circuit.rx(-math.pi/2, 0)
 else:
     raise ValueError("Invalid operator: Must be Z, Y, or RX")
 circuit.h(1)
-circuit.h(0)
+circuit.h(2)
 if args.operator == "Z":
-    circuit.cz(1, 2)
+    circuit.cz(1, 0)
 elif args.operator == "Y":
-    circuit.cy(1, 2)
+    circuit.cy(1, 0)
 else:
     raise ValueError("Invalid operator: Must be Z, Y, or RX")
 
-# 2-qubit QFT:
-circuit.h(0)
-circuit.cp(-math.pi/2, 1, 0)
-circuit.h(1)
-circuit.swap(0, 1)
+# 2-qubit QFT, either using builtin or manual implementation:
+if args.qft:
+    circuit.append(QFT(2).inverse(), qubits[1:3])
+else:
+    circuit.h(2)
+    circuit.cp(-math.pi/2, 1, 2)
+    circuit.h(1)
+    circuit.swap(1, 2)
 
 # measure:
-circuit.measure(1, 1)
-circuit.measure(0, 0)
+circuit.measure(2, 1)
+circuit.measure(1, 0)
 
 if args.filename:
     circuit.draw(output="mpl", filename=args.filename)
