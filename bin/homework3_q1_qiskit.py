8121e81ff620d3ee9491ede307d43542929e9bee
John Hurst <john.b.hurst@gmail.com>
2024-05-20 09:19:11 +1200

    feat: Added support for theta, phi ranges for bulk accuracy check on teleportation.

--- bin/homework3_q1_qiskit.py
+++ bin/homework3_q1_qiskit.py
@@ -16,6 +16,11 @@ parser.add_argument('--provider', type=str, default='aer', help='Provider (aer, 
 parser.add_argument("--shots", type=int, default=1024, help="Number of shots")
 parser.add_argument("--theta", type=str, default="0.0", help="Theta angle as a fraction of pi, e.g., 'pi/2'")
 parser.add_argument("--phi", type=str, default="0.0", help="Phi angle as a fraction of pi, e.g., '-pi/4'")
+parser.add_argument("--start-theta", type=str, default="0.0", help="Start theta angle as a fraction of pi, e.g., 'pi/2'")
+parser.add_argument("--start-phi", type=str, default="0.0", help="Start phi angle as a fraction of pi, e.g., '-pi/4'")
+parser.add_argument("--step-theta", type=str, default="pi/6", help="Step theta angle as a fraction of pi, e.g., 'pi/12'")
+parser.add_argument("--step-phi", type=str, default="pi/6", help="Step phi angle as a fraction of pi, e.g., 'pi/12'")
+parser.add_argument("--steps", type=int, default=0, help="Number of steps")
 parser.add_argument("--realign", action="store_true", help="Realign qubit 2 to measure as original state of qubit 0")
 parser.add_argument("--proportion", action="store_true", help="Proportion of 0 (correct) states")
 parser.add_argument("--filename", type=str, help="Filename for circuit diagram")
@@ -27,48 +32,63 @@ def safe_eval(expr):
 
 theta = safe_eval(args.theta)
 phi = safe_eval(args.phi)
+start_theta = safe_eval(args.start_theta)
+start_phi = safe_eval(args.start_phi)
+step_theta = safe_eval(args.step_theta)
+step_phi = safe_eval(args.step_phi)
 
-qubits = QuantumRegister(3)
-clbits = ClassicalRegister(3)
-circuit = QuantumCircuit(qubits, clbits)
-(q0, q1, q2) = qubits
-(c0, c1, c2) = clbits
+def run(theta, phi):
+    qubits = QuantumRegister(3)
+    clbits = ClassicalRegister(3)
+    circuit = QuantumCircuit(qubits, clbits)
+    (q0, q1, q2) = qubits
+    (c0, c1, c2) = clbits
 
-circuit.ry(theta, q0)
-circuit.rz(phi, q0)
-circuit.h(q1)
-circuit.cx(q1, q2)
-circuit.cx(q0, q1)
-circuit.h(q0)
-circuit.measure(q0, c0)
-circuit.measure(q1, c1)
-circuit.x(q2).c_if(c1, 1)
-circuit.z(q2).c_if(c0, 1)
-if args.realign:
-    circuit.rz(-phi, q2)
-    circuit.ry(-theta, q2)
-circuit.measure(q2, c2)
+    circuit.ry(theta, q0)
+    circuit.rz(phi, q0)
+    circuit.h(q1)
+    circuit.cx(q1, q2)
+    circuit.cx(q0, q1)
+    circuit.h(q0)
+    circuit.measure(q0, c0)
+    circuit.measure(q1, c1)
+    circuit.x(q2).c_if(c1, 1)
+    circuit.z(q2).c_if(c0, 1)
+    if args.realign:
+        circuit.rz(-phi, q2)
+        circuit.ry(-theta, q2)
+    circuit.measure(q2, c2)
 
-if args.provider == 'aer':
-    backend = AerSimulator()
+    if args.provider == 'aer':
+        backend = AerSimulator()
+    else:
+        backend = next(iter([backend for backend in FakeProvider().backends() if backend.name == args.provider]), None)
+
+    sampler = Sampler(backend)
+
+    pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
+    isa_circuit = pm.run(circuit)
+
+    if args.filename:
+        isa_circuit.draw(output="mpl", style='iqp', idle_wires=False, cregbundle=False, filename=args.filename)
+
+    job = sampler.run([isa_circuit], shots=args.shots)
+    result = job.result()
+    counts = list(result[0].data.values())[0].get_counts()
+    count0 = counts.get("000", 0) + counts.get("001", 0) + counts.get("010", 0) + counts.get("011", 0)
+    count1 = counts.get("100", 0) + counts.get("101", 0) + counts.get("110", 0) + counts.get("111", 0)
+    return (count0, count1)
+
+if args.steps == 0:
+    (count0, count1) = run(theta, phi)
+    if args.proportion:
+        print(f"{count0 / args.shots}")
+    else:
+        print(f"{{0: {count0}, 1: {count1}}}")
 else:
-    backend = next(iter([backend for backend in FakeProvider().backends() if backend.name == args.provider]), None)
-
-sampler = Sampler(backend)
-
-pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
-isa_circuit = pm.run(circuit)
-
-if args.filename:
-    isa_circuit.draw(output="mpl", style='iqp', idle_wires=False, cregbundle=False, filename=args.filename)
-
-job = sampler.run([isa_circuit], shots=args.shots)
-result = job.result()
-counts = result[0].data.c0.get_counts()
-count0 = counts.get("000", 0) + counts.get("001", 0) + counts.get("010", 0) + counts.get("011", 0)
-count1 = counts.get("100", 0) + counts.get("101", 0) + counts.get("110", 0) + counts.get("111", 0)
-
-if args.proportion:
-    print(f"{count0 / args.shots}")
-else:
-    print(f"{{0: {count0}, 1: {count1}}}")
+    for i in range(args.steps):
+        theta = start_theta + i * step_theta
+        for j in range(args.steps):
+            phi = start_phi + j * step_phi
+            (count0, count1) = run(theta, phi)
+            print(f"{{{theta:.4f},{phi:.4f},{count0/args.shots:.4f}}},", flush=True)
